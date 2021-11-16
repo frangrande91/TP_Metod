dc2c219f9935046386c9481b800adf4a0ec063a9
NahuelSalomon <nahuelmdp@hotmail.com>
2021-11-15 21:35:44 -0300

    Starting task app

--- tp_metod/settings.py
+++ tp_metod/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+
     },
 ]
 
